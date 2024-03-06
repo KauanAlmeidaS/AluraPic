fc740110e3c840d37e6938a428b0ad6859ca88d5
KauanAlmeidaS <kauanalmeidasilva1405@gmail.com>
2024-03-06 16:50:41 -0300

    Implementando a lógica do Grid

--- alurapic/src/app/photos/photo-list/photos/photos.component.ts
+++ alurapic/src/app/photos/photo-list/photos/photos.component.ts
@@ -9,10 +9,21 @@ import { Photo } from '../../photo/photo';
 export class PhotosComponent implements OnInit {
 
   @Input() photos: Photo[] = [];
+  rows = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.rows = this.groupColumns(this.photos);
+
   }
 
+  groupColumns(photos: Photo[]) {
+    const newRows = [];
+
+    for(let index = 0; index < photos.length; index+=3) {
+      newRows.push(photos.splice(index, index + 3));
+    }
+    return newRows;
+  }
 }
